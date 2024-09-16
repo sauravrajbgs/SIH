8b5157617b2be1bf518c149a39ebcb9a21b4bb31
saurav kumar <sauravkumar8887252285@gmail.com>
2024-09-17 04:15:00 +0530

    spotlight1

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -50,75 +50,105 @@ function Spotlight() {
        
         
       ];
-    //grid-cols-1 md:grid-cols-3 gap-8   border-gray-300 rounded-md  p-x-3  gap-600
-  return (
-    <div className="bg-[#FF7A0014] h-[943px]  p-[48px]w-full flex gap-28">
-    {/* <div className="container px-4 py-7 bg-black">  */}
-    <div className=" w-[608px] h-[943px] ">
-        
-      <h1 className="text-3xl font-bold mb-8 ml-8 " >Spotlight</h1>
-      
-       
-       
-     
-
-      
-      
-
-        {/* <div className="md:col-span-2 "> */}
-          {/* Wrap Image in an <a> tag to make it clickable */}
-          <a href="https://economictimes.indiatimes.com/news/politics-and-nation/india-post-to-release-stamps-on-ramayana-mahabharata-ravi-shankar-prasad/articleshow/48775918.cms?from=mdr" target="_blank" rel="noopener noreferrer">
-          
+      return(
+        <>
+        < div className='w-full h-[943.95px] bg-[#FF7A0014]'>
+        <div className=''>
+          <h1 className='mx-5  text-2xl font-bold mt-10 ml-5'>Spotlight</h1>
+        </div>
+        <div className='ml-5 flex justify-between border-b pt-4 py-2 border-gray-300 border-opacity-10 '>
+<div className='flex' >
+  <ul className='flex gap-4'>
+    <li>All</li>
+    <li>News</li>
+    <li>Interviews</li>
+    <li>Insights</li>
+  </ul>
+</div>
+<div className='pr-3'>
+  <p>View latest post --</p>
+</div>
+        </div>
+        <div className='w-full h-[807px] flex gap-32 mt-4'>
+          <div className='w-[608px] h-[807px] ml-12'>
+          <div className="md:col-span-2">
             <img
-              className="rounded-md  cursor-pointer mt-3  w-[608px] h-[400px] ml-10"
+              className="rounded-lg w-[608px] h-auto"
               src={posts[0].imageUrl}
               alt={posts[0].title}
             />
-          </a>
-          <p className="text-sm  mt-2 text-red-500 ml-5">Powered by Stampick</p>
-          <h2 className="text-2xl font-bold mt-4 text-center">{posts[0].title}</h2>
-          
-          <p className="mt-2 text-gray-300 text-center">{posts[0].description}</p>
-        </div>
-
-    
-      
-        {/* Sidebar Articles */}
-        <div className='w-[740px] h-[943px] mt-20 '>
-          {posts.slice(1).map((post, index) => (
-          
-            <div key={index} className="mb-1">
-               
-              <h3 className="text-sm text-yellow-400 uppercase   ">
-                {post.category}
-              </h3>
-              <div className='flex gap-3'>
-            <div>
-                  <h4 className="text-md font-semibold ">{post.title}</h4>
-             </div>
-              <div className="flex space-x-4 ">
-                < a href={post.link}>
-                <img
-                  className=" object-cover rounded cursor-pointer w-[240px] h-[115px] "
-                 
-                  alt={post.title}
-                  src={post.imageUrl}
-                />
-                </a>
-               
+             <p className="text-sm text-[#783A73] mt-2">Powered by Stampick</p>
+            <h2 className="text-4xl font-bold mt-4">{posts[0].title}</h2>
+           
+            <p className=" text-gray-300 mt-3">{posts[0].description}</p>
+          </div>
+          </div>
+          <div className='w-[635px] h-[807px] '>
+           
+            <div className='flex gap-3 mt-6 '>
+              <div className='border-b pt-4 py-2  border-gray-300 border-opacity-10'>
+                <div className='mt-2 text-[#786B3A]'>SPOTLIGHT</div>
+                <div className='text-1.2xl'>
+                  <h4>Britain's Royal Mail celebrates 65 Britain's Royal Mail celebrates 65 </h4>
+                </div>
+              </div>
+              <div className='  '>
+                <img className='h-[115px] w-[240px] rounded-[14px]' src='https://www.linns.com/images/default-source/news/great-britain-2023-paddington-bear-stamps.jpg?sfvrsn=6c070ad1_4'/>
               </div>
             </div>
+            <div className='flex gap-3 mt-6 '>
+              <div className='border-b pt-4 py-2  border-gray-300 border-opacity-10'>
+                <div className='mt-2 text-[#786B3A]'>SPOTLIGHT</div>
+                <div className='text-1.2xl'>
+                  <h4>Britain's Royal Mail celebrates 65 Britain's Royal Mail celebrates 65 </h4>
+                </div>
+              </div>
+              <div className='  '>
+                <img className='h-[115px] w-[240px] rounded-[14px]' src='https://www.linns.com/images/default-source/news/great-britain-2023-paddington-bear-stamps.jpg?sfvrsn=6c070ad1_4'/>
+              </div>
             </div>
-          ))}
-          {/* <div className="mt-4 text-yellow-400 hover:underline">
-            <a href="#">View all posts →</a>
-          </div> */}
-        </div>
-      </div>
-    
-  
+            <div className='flex gap-3 mt-6 '>
+              <div className='border-b pt-4 py-2  border-gray-300 border-opacity-10'>
+                <div className='mt-2 text-[#786B3A]'>SPOTLIGHT</div>
+                <div className='text-1.2xl'>
+                  <h4>Britain's Royal Mail celebrates 65 Britain's Royal Mail celebrates 65 </h4>
+                </div>
+              </div>
+              <div className='  '>
+                <img className='h-[115px] w-[240px] rounded-[14px]' src='https://www.linns.com/images/default-source/news/great-britain-2023-paddington-bear-stamps.jpg?sfvrsn=6c070ad1_4'/>
+              </div>
+            </div>
+            <div className='flex gap-3 mt-6 '>
+              <div className='border-b pt-4 py-2  border-gray-300 border-opacity-10'>
+                <div className='mt-2 text-[#786B3A]'>SPOTLIGHT</div>
+                <div className='text-1.2xl'>
+                  <h4>Britain's Royal Mail celebrates 65 Britain's Royal Mail celebrates 65 </h4>
+                </div>
+              </div>
+              <div className='  '>
+                <img className='h-[115px] w-[240px] rounded-[14px]' src='https://www.linns.com/images/default-source/news/great-britain-2023-paddington-bear-stamps.jpg?sfvrsn=6c070ad1_4'/>
+              </div>
+            </div>
+            <div className='flex gap-3 mt-6 '>
+              <div className='border-b pt-4 py-2  border-gray-300 border-opacity-10'>
+                <div className='mt-2 text-[#786B3A]'>SPOTLIGHT</div>
+                <div className='text-1.2xl'>
+                  <h4>Britain's Royal Mail celebrates 65 Britain's Royal Mail celebrates 65 </h4>
+                </div>
+              </div>
+              <div className='  '>
+                <img className='h-[115px] w-[240px] rounded-[14px]' src='https://www.linns.com/images/default-source/news/great-britain-2023-paddington-bear-stamps.jpg?sfvrsn=6c070ad1_4'/>
+              </div>
+            </div>
 
-  )
+          </div>
+        </div>
+        </div>
+        </>
+      )
+    
+ 
 }
+  
 
 export default Spotlight
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 function Subscribe() {
   return (
-    <div className="relative bg-[#1A3A47] text-white py-8 px-6 md:px-10 rounded-lg w-full mt-5 mx-auto">
+    <div className="relative bg-[#1A3A47] text-white py-8 px-6 md:px-10 rounded-lg w-full height-[242px]  mt-5 mx-auto">
       <div className="flex flex-col md:flex-row md:justify-space-around gap-64 md:items-center">
-        {/* Left Side: Title and Subtitle */}
+        
         <div className="mb-6 md:mb-0 text-left">
           <h2 className="text-2xl font-bold">Stay in the loop</h2>
           <p className="text-lg">Get the latest insights</p>
         </div>
 
-        {/* Right Side: Form */}
+        
         <div className="max-w-lg w-full">
-          <form className="flex gap-4">
+          <form className="flex gap-4 ">
             <input
               type="email"
               placeholder="Your e-mail"
-              className="py-3 px-4 rounded-l-lg w-418px h-48px bg-gray-800 text-gray-300 placeholder-gray-400 focus:outline-none"
+              className="py-[15.96px]  rounded-[16px] px-[15px] w-[418px] h-[48px] bg-gray-800 text-gray-300 placeholder-gray-400 focus:outline-none"
             />
             <button
               type="submit"
-              className="bg-white text-gray-800 px-6 py-3 rounded-r-lg font-semibold hover:bg-gray-200"
+              className= " text-black py-[9px] px-[19px] bg-white w-[100px] h-[48px]  rounded-[16px] border-[1px] font-semibold hover:bg-gray-200"
             >
               Sign up
             </button>
