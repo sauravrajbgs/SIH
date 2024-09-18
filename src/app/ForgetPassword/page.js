4426c1df7403ddf68e3bd3d71156b072c181ae16
anandzam <anand@gochoo.in>
2024-09-19 02:32:30 +0530

    page to Page

--- src/app/ForgetPassword/page.js
+++ src/app/ForgetPassword/page.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useState } from 'react';
 
-function page() {
+function Page() {
     const [emailSent, setEmailSent] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -114,4 +114,4 @@ function page() {
   );
 }
 
-export default page
+export default Page
